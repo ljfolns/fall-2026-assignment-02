d3393131cca2426854e450ceb854bb07d85743aa
z-tyn <zackwtyner@gmail.com>
2026-09-03 15:51:38 -0400

    feat: Skeleton code, implemented async function for getCategoryBudgets

--- src/strategies/BudgetLimitStrategy.ts
+++ src/strategies/BudgetLimitStrategy.ts
@@ -13,10 +13,28 @@ export class BudgetLimitStrategy implements AuditStrategy {
   ): Promise<string> {
     // TODO: Feature 1 - Implement this strategy.
     // 1. Call BudgetService.getCategoryBudgets() asynchronously.
+    async function getCatBudgets(): Promise<T> {
+      try {
+        const result = await BudgetService.getCategoryBudgets();
+        return result;
+      }
+      catch(error){
+        throw new Error('Operation not possible');
+      }
+    }
     // 2. Group expenses (amounts < 0) by category and compute total spending for each category.
+      //map arrow function of getCatBudgets -> new array of sorted values
+        const mappedCategories = getCatBudgets.map((amounts) => amounts < 0);
+      //make new array of summed total spending of each category
+        const summedTotals = mappedCategories.map((x) => );
     // 3. Compare spending against the fetched limits.
+      //array of booleans, true if spending > limits
     // 4. Identify overages (categories where spending exceeds the budget).
+      //array of bools fulfills this request
+      const overageArr = 
     // 5. Format and return a text-based audit report outlining limits, actuals, overage amounts, percentages, and lists of transactions causing the overage.
+      //make arr of transactions causing overages
+      const audit = "Limits: \nActuals: \nOverage Amounts: \nPercentages: \nTransactions causing overages:" `${overageArr}`;
 
     throw new Error('Method not implemented.');
   }
